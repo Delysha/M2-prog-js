d2bff84b356dc7c40d16fa4c4d2b6b0c9eccf9d4
delysha <34533@ma-web.nl>
2022-11-22 12:31:43 +0100

    les1

---
+++ les1/app.js
@@ -0,0 +1,18 @@
+class App
+{
+ runApplication()
+ {
+    console.log("hello world!");
+    let appNaam = "Magister";
+    let versieNummer = 0.9; 
+    let  author =  "Delysha Broertjes"    
+    let copyright = "This is made by Del" 
+    let distributeur = "MediaCollege"
+    Darkmode = true
+    console.log(appNaam);
+ }
+
+
+}
+let app =new App();
+app.runApplication();
