abf45bc93758547f9d7bf847348f6fe49677f5c9
delysha <34533@ma-web.nl>
2022-11-24 11:38:05 +0100

    pizza

--- les1/app.js
+++ les1/app.js
@@ -5,16 +5,19 @@ class App
     console.log("hello world!");
     let appNaam = "Magister";
     let versieNummer = 0.9; 
-    let  author =  "Delysha Broertjes"    
-    let copyright = "This is made by Del" 
-    let distributeur = "MediaCollege"
-    Darkmode = true
+    let  author =  "Delysha Broertjes";    
+    let copyright = "This is made by Del";
+    let distributeur = "MediaCollege";
+    let Darkmode = true;
     console.log(appNaam);
+    console.log (versieNummer);
+    console.log(author);
+    console.log(copyright);
+    console.log(distributeur);
+    console.log(Darkmode);
  }
 
 
 }
 let app =new App();
 app.runApplication();
-
-console.log(runApplication)
